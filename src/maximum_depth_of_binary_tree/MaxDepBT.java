8060c05c30be36e46051b46dd140319cae03a0ba
han1254 <you@example.com>
2020-04-05 07:49:28 +0800

    最大二叉树深度

---
+++ src/maximum_depth_of_binary_tree/MaxDepBT.java
@@ -0,0 +1,59 @@
+package maximum_depth_of_binary_tree;
+
+/**
+ * https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/description/?utm_source=LCUS&utm_medium=ip_redirect_q_uns&utm_campaign=transfer2china
+ * 104. 二叉树的最大深度
+ * 给定一个二叉树，找出其最大深度。
+ *
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例：
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回它的最大深度 3 。
+ */
+public class MaxDepBT {
+
+    static int left;
+    static int right;
+    static int max;
+    static class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode(int x) { val = x; }
+    }
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(9);
+        TreeNode node3 = new TreeNode(20);
+        TreeNode node4 = new TreeNode(15);
+        TreeNode node5 = new TreeNode(7);
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = null;
+        node2.right = null;
+        node3.left = node4;
+        node3.right = node5;
+        node4.left = null;
+        node4.right = null;
+        node5.left = null;
+        node5.right = null;
+        System.out.println(maxDepth(node1));
+    }
+
+    public static int maxDepth(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(node.left), maxDepth(node.right)) + 1;
+    }
+
+}
